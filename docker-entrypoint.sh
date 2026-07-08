876ff8ebf0cdef3db103a1263ba58e193f9da73e
JIAN-LESTER <143403737+JIAN-LESTER@users.noreply.github.com>
2026-07-09 00:22:03 +0800

    Add render.yaml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 php artisan migrate --force
-php artisan db:seed --class=BukidnonLocationsSeeder --force
-php artisan weather:store-forecasts
+php artisan db:seed --class=BukidnonLocationsSeeder --force || true
+php artisan weather:store-forecasts || true
 php artisan schedule:work >> storage/logs/scheduler.log 2>&1 &
 
 exec php artisan serve --host=0.0.0.0 --port=8000
